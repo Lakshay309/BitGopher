bf5213d514a8f24fe49c7ec3ee405d8ab6382298
Lakshay Kamboj <lkamboj2300@gmail.com>
2026-07-23 22:15:01 +0530

    working tcp server phase 1

--- cmd/bitgopher/main.go
+++ cmd/bitgopher/main.go
@@ -4,30 +4,33 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/Lakshay309/bitgopher/internal/common"
+	"github.com/Lakshay309/bitgopher/internal/discovery"
+	"github.com/Lakshay309/bitgopher/internal/peer"
 	"github.com/Lakshay309/bitgopher/internal/transport"
 	"github.com/google/uuid"
 )
 
 func main() {
 	// tcp server starts first
-	// var i common.DiscoveryMode = 1
-	// port := ":8082"
+	var i common.DiscoveryMode = 1
+	port := ":8082"
 
-	// peerManager := peer.NewPeerManager(i, 10)
+	peerManager := peer.NewPeerManager(i, 10)
 
-	// go peerManager.Run()
+	go peerManager.Run()
 
-	// discoveryServer, err := discovery.NewUdpServer(port, i, peerManager.DiscoveryChan, peerManager.RemoverChan, "hu")
+	discoveryServer, err := discovery.NewUdpServer(port, i, peerManager.DiscoveryChan, peerManager.RemoverChan, "hu")
 
-	// if err != nil {
-	// 	fmt.Println("error creating udp server")
-	// }
+	if err != nil {
+		fmt.Println("error creating udp server")
+	}
 
-	// discoveryServer.PeerID = peerManager.Self.ID
+	discoveryServer.PeerID = peerManager.Self.ID
 
-	// go discoveryServer.Start()
+	go discoveryServer.Start()
 
-	// time.Sleep(10 * time.Second)
+	time.Sleep(10 * time.Second)
 
 	tcp1, err := transport.NewTCPTransport(":8080", uuid.New())
 	if err != nil {
@@ -43,7 +46,7 @@ func main() {
 
 	tcp1.Connect("localhost:8081")
 
-	time.Sleep(5* time.Second)
+	time.Sleep(5 * time.Second)
 
 	fmt.Println("TCP1 Connections:", tcp1.ConnectionCount())
 	fmt.Println("TCP2 Connections:", tcp2.ConnectionCount())
